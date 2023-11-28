56d93e14cc6e9b177de4bd3a9579624c77578bf1
agismandala007 <agixsatria@gmail.com>
2023-11-28 21:16:56 +0700

    install breze

---
+++ database/migrations/2023_11_27_064046_create_camp_benefits_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+
+            //Cara pertama dalam melakukan realsional database
+            // $table->bigInteger('camp_id')->unsigned();
+            // $table->unsignedBigInteger('camp_id');
+            // $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+
+
+            $table->id();
+            $table->foreignId('camp_id')->constrained();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+};
